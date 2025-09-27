e844110b1ef0ddcaec34b446374cf657604851e8
Yash Rana <ranayash35495@gmail.com>
2025-09-27 14:24:44 +0000

    mongoose data modeling

---
+++ 03_mongoose/models/ecommerce/product.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    productImage: { type: String, required: true }, // Buffer(like image, audio, video, PDF) type value make database heavy that why we always store Buffer value in local server machine or online bucket(storage)
+    price: { type: Number, default: 0 },
+    stock: { type: Number, default: 0 },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
---
+++ 03_mongoose/models/hospital-management/doctor.models.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const doctorSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    salary: { type: String, required: true },
+    qualification: { type: String, required: true },
+    exprienceInYears: { type: Number, default: 0 },
+    worksInHospitals: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Hospital" }],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Doctor = mongoose.model("Doctor", doctorSchema);
